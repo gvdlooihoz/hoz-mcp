7960d8f99f32a7502b2dc7c5cb44719431a15715
Gerard van de Looi <gvdlooi@gmail.com>
2025-03-31 14:32:04 +0200

    Deleted compiles code in dist dirtectory

--- dist/functions/index.js
+++
@@ -1,9 +0,0 @@
-export { GetScheduleFunction } from './getschedule.function';
-export { IsCustomerFunction } from './iscustomer.function';
-export { RegisterLessonFunction } from './registerlesson.function';
-export { CancelBookedLessonFunction } from './cancelbookedlesson.function';
-export { RegisterEventFunction } from './registerevent.function';
-export { CancelBookedEventFunction } from './cancelbookedevent.function';
-export { IsTherapistFunction } from './istherapist.function';
-export { BookRoomFunction } from './bookroom.function';
-export { CancelBookedRoomFunction } from './cancelbookedroom.function';
